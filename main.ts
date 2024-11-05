4365f0cb4a0b440481e2fc09538c2a59d7e1d643
Charlotte-techniek <charlotte.malfliet99@gmail.com>
2024-11-05 10:29:45 +0100

    omschrijf wanneer hij dood is

--- main.ts
+++ main.ts
@@ -6,15 +6,15 @@ input.onButtonPressed(Button.B, function () {
 })
 let knuffelen = 20
 let voeden = 20
-let dood = 1
+let dood = 0
 loops.everyInterval(1000, function () {
     knuffelen += -1
     voeden += -1
 })
 basic.forever(function () {
-    if (voeden < 10 && voeden > 1 && (knuffelen > 10 && knuffelen < 1)) {
+    if (voeden < 10 && voeden > 1 || knuffelen > 10 && knuffelen < 1) {
         basic.showIcon(IconNames.Sad)
-    } else if (dood == 1) {
+    } else if (false) {
         basic.showIcon(IconNames.Asleep)
     } else if (voeden > 10 && knuffelen > 10) {
         basic.showIcon(IconNames.Happy)
