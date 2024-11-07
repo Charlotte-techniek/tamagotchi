74eedf6b0157c135640d4e3ea9957a0abc5ff1d6
Charlotte-techniek <charlotte.malfliet99@gmail.com>
2024-11-07 10:09:33 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,21 +1,28 @@
 input.onButtonPressed(Button.A, function () {
-    knuffelen += 5
+    if (knuffelen > 0) {
+        knuffelen += 5
+    }
 })
 input.onButtonPressed(Button.B, function () {
-    voeden += 5
+    if (voeden > 0) {
+        voeden += 5
+    }
 })
-let knuffelen = 20
-let voeden = 20
+let voeden = 0
+let knuffelen = 0
+knuffelen = 20
+voeden = 20
 let dood = 0
 loops.everyInterval(1000, function () {
     knuffelen += -1
     voeden += -1
 })
 basic.forever(function () {
-    if (voeden < 10 && voeden > 1 || knuffelen > 10 && knuffelen < 1) {
+    if (voeden < 10 && voeden > 1 || knuffelen < 10 && knuffelen > 1) {
         basic.showIcon(IconNames.Sad)
-    } else if (false) {
+    } else if (knuffelen < 1 || voeden < 1) {
         basic.showIcon(IconNames.Asleep)
+        dood = 1
     } else if (voeden > 10 && knuffelen > 10) {
         basic.showIcon(IconNames.Happy)
     }
